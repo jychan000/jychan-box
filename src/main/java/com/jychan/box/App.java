bc43bb4ca85268cbdf04888af2345d4d8612fec4
Raymond <415683089@qq.com>
2015-12-05 18:37:37 +0800

    add src d

---
+++ src/main/java/com/jychan/box/App.java
@@ -0,0 +1,15 @@
+package com.jychan.box;
+
+
+/**
+ * @author Raymond
+ * @version 15/12/5 18:33 .
+ * @E-mail:415683089@qq.com
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
