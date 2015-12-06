f16a3825894f4d74dadc96a7b21e6fc02cb543b5
Raymond <415683089@qq.com>
2015-12-06 20:28:34 +0800

    boot hello

---
+++ src/main/java/com/jychan/box/springboot/example/ExampleAppController.java
@@ -0,0 +1,16 @@
+package com.jychan.box.springboot.example;
+
+import org.springframework.boot.CommandLineRunner;
+
+/**
+ * @author Raymond
+ * @version 15/12/6 20:20 .
+ * @E-mail:415683089@qq.com
+ */
+public class ExampleAppController implements CommandLineRunner {
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+    }
+}
--- src/main/java/com/jychan/box/springboot/example/ExampleWebController.java
+++ src/main/java/com/jychan/box/springboot/example/ExampleWebController.java
@@ -1,4 +1,4 @@
-package com.jychan.box.boot;
+package com.jychan.box.springboot.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -8,23 +8,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Raymond
- * @version 15/12/6 00:02 .
+ * @version 15/12/6 20:15
  * @E-mail:415683089@qq.com
  */
 @Controller
 @EnableAutoConfiguration
-public class SampleController {
+public class ExampleWebController {
 
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        System.out.println("haha...");
+        System.out.println("haha...>>");
         return "Hello World!";
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(SampleController.class, args);
+        SpringApplication.run(ExampleWebController.class, args);
     }
-
-
 }
